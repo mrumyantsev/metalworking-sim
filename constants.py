82d30becb9f02ad3688678a693efe1b7682c2db8
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2023-04-06 21:47:18 +0300

    chore: delete unused constants file

--- constants.py
+++
@@ -1,9 +0,0 @@
-cfg = {
-    'ent_backgroundColor': (0, 0, 0),
-    'ent_millPlateColor': (255, 255, 0),
-    'ent_steelColor': (192, 192, 192),
-    'ent_trajectoryColor': (255, 0, 0),
-    'r_fps': 91,
-    'r_resolutionHeight': 960,
-    'r_resolutionWidth': 1600,
-}
