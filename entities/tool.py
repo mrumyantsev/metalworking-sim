431ca0c1e9c78640fd6299f33c1fc2a94e4a06b9
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2023-04-07 17:04:19 +0300

    chore(entities): rename field that keeps trajectory points

--- entities/tool.py
+++ entities/tool.py
@@ -7,15 +7,15 @@ import libs.lib as lib
 class Trajectory:
     def __init__(self, display_surface: pygame.Surface) -> None:
         self.__display_surface = display_surface
-        self.__trajectory_path = sll.Sll()
+        self.__points_list = sll.Sll()
     
     def add_point(self, x: float, y: float) -> None:
         new_point = (lib.pbround(x), lib.pbround(y))
 
-        self.__trajectory_path.add_to_tail(new_point)
+        self.__points_list.add_to_tail(new_point)
 
     def draw(self, color: tuple) -> None:
-        self.__trajectory_path.traverse_from_head(
+        self.__points_list.traverse_from_head(
             self.__display_surface.set_at, color)
 
 
