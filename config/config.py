f890a26cd48907db30f6be5634b84d9fe7a77997
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2023-04-21 20:16:17 +0300

    refactor(lib): move color converting function from config module

--- config/config.py
+++ config/config.py
@@ -4,9 +4,3 @@ import yaml
 def load_config(path: str) -> dict:
     with open(path, 'r') as file:
         return yaml.safe_load(file)
-
-
-def get_color_value(config_value: str) -> tuple:
-    rgb_strings = config_value.split(' ')
-    rgb_ints = [int(c) for c in rgb_strings]
-    return tuple(rgb_ints)
--- lib/lib.py
+++ lib/lib.py
@@ -1,3 +1,9 @@
+def get_color_value(config_value: str) -> tuple:
+    rgb_strings = config_value.split(' ')
+    rgb_ints = [int(c) for c in rgb_strings]
+    return tuple(rgb_ints)
+
+
 # Pseudo banker's rounding.
 def pbround(value: float) -> int:
     return round(value + 0.1)
